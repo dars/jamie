690e70ff0ecde8f6a04987e83cf8f080b19e188a
Dars <dars94@gmail.com>
2014-05-18 00:15:38 +0800

    第二位歌手的關聯設定

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
   self.table_name = 'songtable'
   belongs_to :singer1, class_name: 'Singer', primary_key: 'ID', foreign_key: 'SingerIndex1'
+  belongs_to :singer2, class_name: 'Singer', primary_key: 'ID', foreign_key: 'SingerIndex2'
   belongs_to :phylum, class_name: 'Phylum', foreign_key: 'Phylum'
 
   def human_song_number
