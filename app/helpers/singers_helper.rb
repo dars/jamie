a8841a4cc7530f2c83f9a13c7f2a33cf1480a45f
Dars <dars94@gmail.com>
2014-05-18 15:35:38 +0800

    singer index page edit

--- app/helpers/singers_helper.rb
+++ app/helpers/singers_helper.rb
@@ -1,2 +1,13 @@
 module SingersHelper
+  def singer_kind (kind)
+    case (kind)
+      when 1
+        res = raw('<span class="label label-primary">男歌星</span>')
+      when 2
+        res = raw('<span class="label label-danger">女歌星</span>')
+      when 3
+        res = raw('<span class="label label-inverse">團體</span>')
+    end
+    return res
+  end
 end
