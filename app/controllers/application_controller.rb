5fa9ace9cb0ed49d2b3b928c80eb3fba733e3a42
Dars <dars94@gmail.com>
2014-05-22 06:59:51 +0800

    add right sidebar hot songs list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :getHotSongs
   protect_from_forgery with: :exception
   include SessionHelper
   def set_flash(type, message, now = false)
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     else
       flash[:notice] = {:type => type, :message => message}
     end
+  end
+
+  def getHotSongs
+    @hots = Playlog.group('SongNumberID').order('count(id) DESC').limit(5)
   end
 end
