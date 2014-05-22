afbcb77396489d64aa11a14551c5d8eb48f3f593
Dars <dars94@gmail.com>
2014-05-22 18:47:00 +0800

    fix right sidebar issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,8 @@ class ApplicationController < ActionController::Base
   # 計算機器該月貢獻天數
   # target_date = '2014-05-21'
   def alive_days (device_id, target_date)
-    s_date = target_date.year.to_s+'-'+target_date.month.to_s+'-01'
-    e_date = target_date.year.to_s+'-'+target_date.month.to_s+'-'+Time.days_in_month(target_date.month).to_s
+    s_date = target_date.year.to_s+'-'+target_date.strftime('%m')+'-01'
+    e_date = target_date.year.to_s+'-'+target_date.strftime('%m')+'-'+Time.days_in_month(target_date.month).to_s
     @transaction = Transation.where('device_id=?', device_id).where('(? between start_date and end_date) or (? between start_date and end_date)', s_date, e_date).first
     if(@transaction)
       if (@transaction.start_date < Date.new(target_date.year, target_date.month, 1))
